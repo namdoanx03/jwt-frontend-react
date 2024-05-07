b15aeaeb1964665341964dde94a73df2cc0a9967
namdoanx03 <dpnbro1102@gmail.com>
2024-05-08 01:38:40 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.scss';
+import Nav from './components/Navigation/Nav';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Namdoanx
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app-container'>
+          {/* <Nav/> */}
+          <Switch>
+            <Route path="/news">
+              News
+            </Route>
+            <Route path="/about">
+              About
+            </Route>
+            <Route path="/contact">
+             Contact
+            </Route>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path="/" exact>
+              Home
+            </Route>
+            <Route path="*">
+              404 not found
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,10 @@
+
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            Login
+            <button className="btn btn-primary">Test boostrap</button>
+        </div>
+    )
+}
+export default Login
